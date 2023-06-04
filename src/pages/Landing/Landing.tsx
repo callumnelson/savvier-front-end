880802642ae0ac70b28be41f77628b0ddcea3edc
Callum Nelson <callum.nelson95@gmail.com>
2023-06-04 16:37:55 -0400

    remove user from landing props

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -4,15 +4,7 @@ import { Link } from 'react-router-dom';
 // css
 import styles from './Landing.module.css'
 
-// types
-import { User } from '../../types/models'
-
-interface LandingProps {
-  user: User | null;
-}
-
-const Landing = (props: LandingProps): JSX.Element => {
-  const { user } = props
+const Landing = (): JSX.Element => {
 
   return (
     <main className={styles.container}>
